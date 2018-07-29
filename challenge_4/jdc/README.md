2fb31bb9f58881b094655c1b263d5af50a46fc14
Jake Culp <jakeculp@gmail.com>
2018-07-29 17:09:22 -0400

    :sparkles: Add Solution for Challenge 4

---
+++ challenge_4/jdc/README.md
@@ -0,0 +1,9 @@
+# Challenge 4 - How to run
+
+1. `cd` to the root of this folder in terminal
+2. _*If you do not have `gulp` installed on your system run Step 3, if otherwise skip to Step 4*_
+3. Run `npm install --global gulp-cli` in terminal
+4. Run `gulp` to run the default tasks.
+5. Output files with be put into folder named `dist`
+6. Default browser will automatically open `dist/index.html`
+7. Any changes made to `screen.styl` will recompile `.css` and refresh the browser window
---
+++ challenge_4/jdc/gulpfile.js
@@ -0,0 +1,35 @@
+// include the required packages.
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+// Get one .styl file and render
+gulp.task('style-reload', function () {
+  return gulp.src('src/stylus/screen.styl')
+    .pipe(stylus())
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(reload({ stream:true }));
+});
+
+//
+gulp.task('copy', function () {
+  return gulp.src('src/views/index.html')
+      .pipe(gulp.dest('./dist/'));
+});
+
+
+gulp.task('watch',['style-reload','copy'], () => {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    }
+  });
+	gulp.watch('src/stylus/screen.styl', ['default']);
+});
+
+// Default gulp task to run
+gulp.task('default', ['style-reload','copy','watch']);
